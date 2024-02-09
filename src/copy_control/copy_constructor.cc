ae3560a609d903c38c4c2b41d89db62ddbdc3451
Lashi <lashi0812@gmail.com>
2024-02-09 13:07:23 +0000

    added clang format

--- src/copy_control/copy_constructor.cc
+++ src/copy_control/copy_constructor.cc
@@ -2,48 +2,48 @@
 #include <vector>
 
 class Foo {
-public:
-  Foo() = default;
-  Foo(int num) : magic_number(num) {}
-  Foo(const Foo &other) : magic_number(other.magic_number) {
-    std::cout << "Calling Copy Constructor" << std::endl;
-  }
-  int getMagicNumber() { return magic_number; }
+  public:
+    Foo() = default;
+    Foo(int num) : magic_number(num) {}
+    Foo(const Foo &other) : magic_number(other.magic_number) {
+        std::cout << "Calling Copy Constructor" << std::endl;
+    }
+    int getMagicNumber() { return magic_number; }
 
-private:
-  int magic_number = 0;
+  private:
+    int magic_number = 0;
 };
 
 Foo global;
 Foo passingFooAndReturnFoo(Foo other) {
-  std::cout << "Local Copy : ";
-  Foo local = other;
-  std::cout << "Creating Foo Pointer : ";
-  Foo *heap = new Foo(global);
-  std::cout << "Assigning Foo to Foo Pointer : \n";
-  *heap = local;
-  std::cout << "Creating the Foo Array : ";
-  Foo fooArray[4] = {local, *heap};
-  std::cout << "Returning the Foo : ";
-  return *heap;
+    std::cout << "Local Copy : ";
+    Foo local = other;
+    std::cout << "Creating Foo Pointer : ";
+    Foo *heap = new Foo(global);
+    std::cout << "Assigning Foo to Foo Pointer : \n";
+    *heap = local;
+    std::cout << "Creating the Foo Array : ";
+    Foo fooArray[4] = {local, *heap};
+    std::cout << "Returning the Foo : ";
+    return *heap;
 }
 
 class AggregateClass {
-public:
-  Foo some;
-  void get(){};
+  public:
+    Foo  some;
+    void get(){};
 };
 
 int main() {
-  Foo A = Foo(42'000);
-  std::cout << "Assessing the Foo : ";
-  Foo B = A;
-  std::cout << "Passing the Foo : ";
-  passingFooAndReturnFoo(B);
-  std::cout << "Using Foo in Brace initialization of Agg Class: ";
-  AggregateClass agg = {B};
-  std::cout << "Using Foo in Brace initialization of Vector : ";
+    Foo A = Foo(42'000);
+    std::cout << "Assessing the Foo : ";
+    Foo B = A;
+    std::cout << "Passing the Foo : ";
+    passingFooAndReturnFoo(B);
+    std::cout << "Using Foo in Brace initialization of Agg Class: ";
+    AggregateClass agg = {B};
+    std::cout << "Using Foo in Brace initialization of Vector : ";
 
-  std::vector<Foo> fooVector;
-  fooVector.push_back(A);
+    std::vector<Foo> fooVector;
+    fooVector.push_back(A);
 }
